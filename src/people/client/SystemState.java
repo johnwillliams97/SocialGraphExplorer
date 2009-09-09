a33b049829b6c5b00291f8c7e82f3a3be23a803a
peterwilliams97 <peter.williams.97@gmail.com>
2009-09-09 23:31:16 +1000

    Defaulted SystemState.uniqueID to PersonClient.MAGIC_PERSON_CLIENT_1_UNIQUE_ID for consistent behaior

--- src/people/client/SystemState.java
+++ src/people/client/SystemState.java
@@ -9,48 +9,50 @@ public class SystemState {
 	
 	public static final String KEY_UNIQUEID   = "key";
 	public static final String KEY_INDEX = "idx";
-	private long _uniqueID = 0L;
+	private long _uniqueID = PersonClient.MAGIC_PERSON_CLIENT_1_UNIQUE_ID;
 	private int  _index = 0;
 	
 	public SystemState(String stateString) {
 		parseStateString(stateString);
 	}
 	public SystemState(long uniqueID, int index) {
-		this._uniqueID = uniqueID;
-		this._index = index;
+		_uniqueID = uniqueID;
+		_index = index;
 	}
 
 	private static final String SEP_INTER = "&";
 	private static final String SEP_INTRA = "=";
 	
 	public String getAsString() {
-		String termUniqueID = KEY_UNIQUEID + SEP_INTRA + this._uniqueID;
-		String termIndex = KEY_INDEX + SEP_INTRA + this._index;
+		String termUniqueID = KEY_UNIQUEID + SEP_INTRA + _uniqueID;
+		String termIndex = KEY_INDEX + SEP_INTRA + _index;
 		String stateString = termUniqueID + SEP_INTER + termIndex;
 		return stateString;
 	}
 	private void parseStateString(String stateString) {
-		String[] terms = stateString.split(SEP_INTER);
-		if (terms != null) {
-			for (String term: terms) {
-				String[] kv = term.split(SEP_INTRA);
-				if (kv != null && kv.length >= 2) {
-					String key = kv[0];
-					String val = kv[1];
-					if (key.equalsIgnoreCase(KEY_UNIQUEID))
-						this._uniqueID = Long.parseLong(val);
-					else if (key.equalsIgnoreCase(KEY_INDEX))
-						this._index = Integer.parseInt(val);
+		if (stateString != null) {
+			String[] terms = stateString.split(SEP_INTER);
+			if (terms != null) {
+				for (String term: terms) {
+					String[] kv = term.split(SEP_INTRA);
+					if (kv != null && kv.length >= 2) {
+						String key = kv[0];
+						String val = kv[1];
+						if (key.equalsIgnoreCase(KEY_UNIQUEID))
+							_uniqueID = Long.parseLong(val);
+						else if (key.equalsIgnoreCase(KEY_INDEX))
+							_index = Integer.parseInt(val);
+					}
 				}
 			}
 		}
 	}
 	
 	public long getUniqueID() {
-		return this._uniqueID;
+		return _uniqueID;
 	}
 	public int getIndex() {
-		return this._index;
+		return _index;
 	}
 
 }
